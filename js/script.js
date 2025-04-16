9cff9164b48a8e802ce74015895abb26adb0cfeb
Nerort <59502452+Nerort@users.noreply.github.com>
2025-04-16 12:24:19 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.getElementById('burger');
     const nav = document.getElementById('nav');
+    const navLinks = document.querySelectorAll('.header__nav a'); // Получаем все ссылки в меню
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('active');
+    });
+
+    // Добавляем обработчик для каждой ссылки в меню
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Проверяем, открыто ли меню (актуально для мобильной версии)
+            if (nav.classList.contains('active')) {
+                nav.classList.remove('active'); // Сворачиваем меню
+            }
+        });
     });
 });
