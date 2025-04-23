703c00a3e2af708b4cacfbfa3723625a116a77af
Nerort <59502452+Nerort@users.noreply.github.com>
2025-04-23 17:11:27 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -13,49 +13,110 @@ async function initMap() {
         }
     );
     
-    // Добавляем обязательные слои
     map.addChild(new YMapDefaultSchemeLayer());
     map.addChild(new YMapDefaultFeaturesLayer());
 
+    // Данные для маркеров
+    const warehouses = [
+        {
+            coordinates: [37.381105, 56.000201],
+            title: "Шереметьево",
+            image: "img/Sheremetyevo/main.webp",
+            prices: [
+                {
+                    type: "Light Industrial в продаже:",
+                    area: "82 308 м²",
+                    price: "132 000 руб./м²"
+                },
+                {
+                    type: "BigBox в продаже:",
+                    area: "122 135 м²",
+                    price: "96 000 руб./м²"
+                }
+            ],
+            link: "objects/sheremetyevo.html"
+        },
+        {
+            coordinates: [37.833614, 55.828820],
+            title: "Абрамцево",
+            image: "img/Abramcevo/main.jpg",
+            prices: [
+                {
+                    type: "Light Industrial в продаже:",
+                    area: "69 264 м²",
+                    price: "184 800 руб./м²"
+                }
+            ],
+            link: "objects/abramcevo.html"
+        },
+        {
+            coordinates: [38.339922, 55.423288],
+            title: "Альфа Light",
+            image: "img/Alfalight/main.webp",
+            prices: [
+                {
+                    type: "Light Industrial в продаже:",
+                    area: "9 000 м²",
+                    price: "132 000 руб./м²"
+                }
+            ],
+            link: "objects/alfalight.html"
+        },
+        {
+            coordinates: [37.894972, 55.075188],
+            title: "Ольховка",
+            image: "img/Olhovka/main.jpg",
+            prices: [
+                {
+                    type: "Light Industrial в продаже:",
+                    area: "39 962 м²",
+                    price: "132 000 руб./м²"
+                },
+                {
+                    type: "BigBox в продаже:",
+                    area: "43 289 м²",
+                    price: "96 000 руб./м²"
+                }
+            ],
+            link: "objects/olhovka.html"
+        }
+    ];
+
     // Функция для создания маркера
-    function createWarehouseMarker(coordinates, price, site) {
+    function createWarehouseMarker(warehouse) {
         const markerElement = document.createElement('div');
         markerElement.className = 'warehouse-marker';
         
         markerElement.innerHTML = `
-            <a href="https://nerort.github.io/warehouse/objects/${site}">
-            <div class="warehouse-price">${price}</div>
+            <a href="${warehouse.link}">
+                <div class="warehouse-price">${warehouse.title}</div>
             </a>
+            <div class="warehouse-tooltip">
+                <div class="warehouse-tooltip-content">
+                    <a href="${warehouse.link}"><img src="${warehouse.image}" alt="${warehouse.title}"></a>
+                    <h3>${warehouse.title}</h3>
+                    <div class="price-block">
+                        ${warehouse.prices.map(price => `
+                            <div class="price-item">
+                                <span class="price-type">${price.type}</span><br>
+                                ${price.area}<br>
+                                <span class="price-value">${price.price}</span> в т.ч. НДС
+                            </div>
+                        `).join('')}
+                    </div>
+                    <a href="${warehouse.link}" class="btn btn--primary">Подробнее</a>
+                </div>
+            </div>
         `;
         
-        const marker = new YMapMarker({ coordinates }, markerElement);
+        const marker = new YMapMarker({ coordinates: warehouse.coordinates }, markerElement);
         return marker;
     }
 
-    // Добавляем 4 маркера складов
-    map.addChild(createWarehouseMarker(
-        [37.381105, 56.000201], 
-        '112,3 Га',
-        'warehousepres.pdf'
-    ));
-
-    map.addChild(createWarehouseMarker(
-        [37.833614, 55.828820], 
-        '16,7 Га',
-        'warehousepres.pdf'
-    ));
-    
-    map.addChild(createWarehouseMarker(
-        [38.339922, 55.423288], 
-        '2 Га',
-        'warehousepres.pdf'
-    ));
-    
-    map.addChild(createWarehouseMarker(
-        [37.894972, 55.075188], 
-        '21 Га',
-        'warehousepres.pdf'
-    ));
+    // Добавляем маркеры
+    warehouses.forEach(warehouse => {
+        map.addChild(createWarehouseMarker(warehouse));
+    });
 }
 
 initMap();
